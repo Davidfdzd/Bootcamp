5da2aca4c8070ce105edbe2b166b6cf9b9f53d36
David Fernández <david.f.olmo@gmail.com>
2025-06-01 20:11:41 +0200

    Add files via upload

---
+++ airpassengers_analisis.R
@@ -0,0 +1,52 @@
+# Cargar del dataset
+data("AirPassengers")
+
+# Inspeccionar la estructura
+print(class(AirPassengers))     # Debe ser "ts"
+print(summary(AirPassengers))   # Estadísticas descriptivas
+print(start(AirPassengers))     # Inicio de la serie temporal
+print(end(AirPassengers))       # Fin de la serie temporal
+print(frequency(AirPassengers)) # Frecuencia mensual (12)
+
+#Exploración inicial
+
+##Gráfico de la serie temporal
+
+plot(AirPassengers)
+
+##Estadísticas descriptivas
+
+mean(AirPassengers)
+sd(AirPassengers)
+
+#Análisis de tendencia y estacionalidad
+
+descomposicion = decompose(AirPassengers)
+plot(descomposicion)
+
+#Análisis de estacionariedad
+
+library(tseries)
+
+##Autocorrelación y autocorrelación parcial
+
+acf(AirPassengers)
+pacf(AirPassengers)
+
+##Prueba de Dickey-Fuller aumentada
+adf.test(AirPassengers)
+
+#Detección de valores atípicos
+
+boxplot(AirPassengers)
+
+##Marcar picos visualmente
+plot(AirPassengers)
+abline(h = quantile(AirPassengers, 0.95), col = "red", lty = 2)
+
+
+#Interpretación
+##Respecto a la tendencia se aprecia un fuerte crecimiento a lo largo de los años.
+##Estacionalidad: Hay picos que se repiten cada año sobre los meses de verano
+##Estacionariedad: la serie es estacionaria
+##Valores atípicos no encontramos en el boxplot, pero en el autocoorelacion parcial si
